a867eafa2a6b735eaf77552d51c4a78d9d727c31
maks1604 <maks16041991@gmail.com>
2019-04-07 22:38:38 +0300

    Готовое задание1

--- Lesson3/src/lesson3/Main.java
+++ Lesson3/src/lesson3/Main.java
@@ -1,18 +1,63 @@
 package lesson3;
 
 import java.util.Random;
+import java.util.Scanner;
 
 public class Main {
+    static Random random = new Random();
+    static Scanner scanner = new Scanner(System.in);
+    static boolean isStartGame;
 
     public static void main(String[] args) {
-        randomZeroToNine();
+        isStartGame = false;
+        menu();
+
+
 
 
     }
-    static void randomZeroToNine (){
-        Random random = new Random();
-         int number = random.nextInt(10);
-        
+
+    static void menu() {
+        if (isStartGame) {
+            System.out.println("\nПовторить игру еще раз? 1 – да / 0 – нет");
+            int menuPoint = scanner.nextInt();
+
+            switch (menuPoint) {
+                case 1:
+                    compareNumbers();
+                    break;
+                case 0:
+                    return;
+                default:
+                    System.out.println("Вы ввели не правильно");
+                    break;
+            }
+
+        } else {
+            isStartGame = true;
+            compareNumbers();
+        }
+        menu();
+    }
+
+
+    static void compareNumbers() {
+        int number = random.nextInt(10);
+
+        for (int i = 0; i < 3; i++) {
+            System.out.println("Введите число от 0 до 10");
+            int scannerNumber = scanner.nextInt();
+
+            if (number == scannerNumber) {
+                System.out.println("Вы угадали");
+                return;
+            } else {
+                String bigOrSmall = scannerNumber > number ? "больше" : "меньше";
+                System.out.println("Вы ввели число " + bigOrSmall + " загадонного");
+            }
+        }
+
+        System.out.println("Попытки закончились");
     }
 
 }
