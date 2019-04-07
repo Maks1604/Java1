bcf7c8d5b1946c5e55b2e0fd40a7fee160a26f3c
maks1604 <maks16041991@gmail.com>
2019-04-07 21:22:35 +0300

    Задание 1 рандом от 0 до 10

--- Lesson3/src/lesson3/Main.java
+++ Lesson3/src/lesson3/Main.java
@@ -1,8 +1,18 @@
 package lesson3;
 
+import java.util.Random;
+
 public class Main {
 
     public static void main(String[] args) {
+        randomZeroToNine();
+
 
     }
+    static void randomZeroToNine (){
+        Random random = new Random();
+         int number = random.nextInt(10);
+        
+    }
+
 }
