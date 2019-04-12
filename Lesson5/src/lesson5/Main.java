bc2e7e27b9da5d5a322006b574283a558afc12f3
maks1604 <maks16041991@gmail.com>
2019-04-12 22:43:58 +0300

    Готов

--- Lesson5/src/lesson5/Main.java
+++ Lesson5/src/lesson5/Main.java
@@ -3,30 +3,31 @@ package lesson5;
 import java.util.Scanner;
 
 public class Main {
-static Scanner scanner = new Scanner(System.in);
+    static Scanner scanner = new Scanner(System.in);
+
     public static void main(String[] args) {
-        Employee[] employeeArray =  readEmployees();
-WriteEmployeeForAge(employeeArray);
+        Employee[] employeeArray = readEmployees();
+        WriteEmployeeForAge(employeeArray);
     }
 
-    public static Employee[] readEmployees(){
+    public static Employee[] readEmployees() {
         System.out.println("Введите введите количество ввода сотрудников ");
         int countEmployee = scanner.nextInt();
         Employee[] employeeArray = new Employee[countEmployee];
-        for (int i = 0; i <countEmployee ; i++) {
+        for (int i = 0; i < countEmployee; i++) {
             employeeArray[i] = readEmployee();
         }
         return employeeArray;
     }
 
-    public static void WriteEmployeeForAge(Employee[] employees){
+    public static void WriteEmployeeForAge(Employee[] employees) {
         System.out.println("Введите минимальный возраст сотрудника");
         int age = scanner.nextInt();
-        for (Employee employee: employees ) {
-            if (employee.getAge()>=age){
+        for (Employee employee : employees) {
+            if (employee.getAge() >= age) {
                 employee.showConsole();
             }
-            
+
         }
     }
 
