11ad996286d5c081cfaa11d66856371c99fa1fc6
maks1604 <maks16041991@gmail.com>
2019-04-17 19:55:21 +0300

    Задание 3-4

--- Lesson6/src/lesson6/Cat.java
+++ Lesson6/src/lesson6/Cat.java
@@ -3,5 +3,10 @@ package lesson6;
 public class Cat extends Animal {
     public Cat(String color, String name, int age) {
         super(color, name, age);
+        super.maxRunLength = 200;
+        super.maxJumpHeight = 2;
+        super.maxSwimLength = -1;
     }
+
+
 }
--- Lesson6/src/lesson6/Dog.java
+++ Lesson6/src/lesson6/Dog.java
@@ -3,5 +3,8 @@ package lesson6;
 public class Dog extends Animal {
     public Dog(String color, String name, int age) {
         super(color, name, age);
+        super.maxRunLength = 500;
+        super.maxJumpHeight = 0.5f;
+        super.maxSwimLength = 10;
     }
 }
