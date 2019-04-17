55a7b80c98f0c6a965fb4f97af1f0f911112b720
maks1604 <maks16041991@gmail.com>
2019-04-17 19:49:25 +0300

    1 Задание

---
+++ Lesson6/src/lesson6/Cat.java
@@ -0,0 +1,7 @@
+package lesson6;
+
+public class Cat extends Animal {
+    public Cat(String color, String name, int age) {
+        super(color, name, age);
+    }
+}
---
+++ Lesson6/src/lesson6/Dog.java
@@ -0,0 +1,7 @@
+package lesson6;
+
+public class Dog extends Animal {
+    public Dog(String color, String name, int age) {
+        super(color, name, age);
+    }
+}
